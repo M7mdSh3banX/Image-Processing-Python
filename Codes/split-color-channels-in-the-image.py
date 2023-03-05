97872d83af277190a59e5b0ef630e664834663b5
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-05 22:24:16 +0200

    update split-color-channels-in-the-image.py

--- Codes/split-color-channels-in-the-image.py
+++ Codes/split-color-channels-in-the-image.py
@@ -5,12 +5,15 @@ mickey_img = cv2.imread("images/mickey.jpeg")
 B, G, R = cv2.split(mickey_img)
 
 cv2.imshow("Original Image", mickey_img)
+cv2.waitKey(0)
 
 cv2.imshow("Image with blue channel", B)
+cv2.waitKey(0)
 
 cv2.imshow("Image with green channel", G)
+cv2.waitKey(0)
 
 cv2.imshow("Image with red channel", R)
-
 cv2.waitKey(0)
+
 cv2.destroyAllWindows()
