8589a0781aa043a97917ddc66378bc5209141a35
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-05 21:45:36 +0200

    add mickey.jpeg and split-color-channels-in-the-image.py

---
+++ Codes/split-color-channels-in-the-image.py
@@ -0,0 +1,16 @@
+import cv2
+
+mickey_img = cv2.imread("images/mickey.jpeg")
+# Colors stores in the BGR format
+B, G, R = cv2.split(mickey_img)
+
+cv2.imshow("Original Image", mickey_img)
+
+cv2.imshow("Image with blue channel", B)
+
+cv2.imshow("Image with green channel", G)
+
+cv2.imshow("Image with red channel", R)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
