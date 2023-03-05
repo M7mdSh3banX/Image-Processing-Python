7f8f30dc2f19c361f2cf60e9193ce7152a102a9c
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-05 20:43:53 +0200

    add reading-and-display-2images-in-one-window.py

---
+++ Codes/reading-and-display-2images-in-one-window.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+cat_and_dog = cv2.imread("cat and dog.jpg")
+cat_and_dog2 = cv2.imread("cat and dog.jpg")
+
+gumball = cv2.imread("gumball.jpg")
+gumball2 = cv2.imread("gumball.jpg")
+
+# Display two images in the same window
+# concatenate image Horizontally
+horizontal = np.concatenate((cat_and_dog, cat_and_dog2), axis=1)
+
+# concatenate image Vertically
+vertical = np.concatenate((gumball, gumball2), axis=0)
+
+cv2.imshow("Horizontal Screen", horizontal)
+cv2.imshow("Vertical Screen", vertical)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
