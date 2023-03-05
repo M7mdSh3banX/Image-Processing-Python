896b858cbe9afcc086ea50ba8c4d94096a5a18bf
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-05 20:43:39 +0200

    update reading-and-display-image-in-gray-scale-mode.py

--- Codes/reading-and-display-image-in-gray-scale-mode.py
+++ Codes/reading-and-display-image-in-gray-scale-mode.py
@@ -1,7 +1,11 @@
 import cv2
 
-gray_img = cv2.imread("gumball.jpg", 0)
-cv2.imshow("The image in gray scale mode", gray_img)
+# Reading an image in grayscale mode with 2 ways
+gumball = cv2.imread("gumball.jpg", 0)
+shaun_the_sheep = cv2.imread("shaun the sheep.jpg", cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow("Gumball in gray scale mode", gumball)
+cv2.imshow("Shaun the sheep in gray scale mode", shaun_the_sheep)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
